a0a4b6b8ebecf8a34135193303cfe776e3c04286
Chaimaa101 <afkirchaimaa36@gmail.com>
2025-12-23 12:23:06 +0100

    test ci/cd

--- Backend-timyo/app/Http/Controllers/AppointmentController.php
+++ Backend-timyo/app/Http/Controllers/AppointmentController.php
@@ -35,7 +35,7 @@ class AppointmentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreappointmentRequest $request)
+    store(StoreappointmentRequest $request)
     {
         try {
             $data = $request->validated();
